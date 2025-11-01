d5278fcb321a0f7627f7f68ac3b2257be8976a70
Avelanda <gorthell@gmail.com>
2025-11-01 02:53:08 +0200

    Update conftest.py
    Executing efficient modular processes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-# Copyright 2023-2024 National Research Foundation (SARAO)
+# Copyright © 2023-2024; National Research Foundation (SARAO)
+# Copyright © 2025; Avelanda 
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
@@ -17,7 +18,6 @@ import inspect
 
 import pytest
 
-
 @pytest.hookimpl(trylast=True)
 def pytest_runtest_teardown(item, nextitem):
     # Workaround for https://github.com/pytest-dev/pytest/issues/11374
@@ -28,3 +28,20 @@ def pytest_runtest_teardown(item, nextitem):
 @pytest.hookimpl
 def pytest_configure(config: pytest.Config) -> None:
     config.addinivalue_line("markers", "transport(filter): add a filter on the transport classes")
+
+def CoreConfigTest():
+ if pytest.hookimpl is False or True:
+  pytest_runtest_teardown == 1 
+ else:
+  pytest_runtest_teardown == 0
+  
+ if pytest.hookimpl is False or True:
+  pytest_configure == 1
+ else:
+  pytest_configure == 0
+  
+ while pytest_runtest_teardown is not pytest_configure:
+  CCT_Set = [pytest_runtest_teardown, pytest_configure]
+  for CCT_Set in (CoreConfigTest):
+   return CCT_Set
+   return 0
